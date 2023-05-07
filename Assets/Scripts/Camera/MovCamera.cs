1056e1d91e0acb8f6eeae9ac9168069958081756
Hyzokaaa <97908828+Hyzokaaa@users.noreply.github.com>
2023-05-07 09:38:33 -0400

    Upd

--- Assets/Scripts/Camera/MovCamera.cs
+++ Assets/Scripts/Camera/MovCamera.cs
@@ -1,25 +1,91 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class MovCamera : MonoBehaviour
 {
-    public Transform target; // referencia al jugador
-    public float distanceY = 14f; // distancia entre la cámara y el jugador
-    public float distanceZ = -14f;
-    public float xRotation = 45f; // rotación en el eje X
-    public float smoothTime = 0.125f;
-    public Vector3 velocity = Vector3.zero;
+    [SerializeField]
+    private Transform target;
+    [Header("Movement")]
+    [SerializeField]
+    private float distanceY;
+    [SerializeField]
+    private float distanceZ;
+    [SerializeField]
+    private float rotationLerp;
+    private Vector3 velocity = Vector3.zero;
+    private float xRotation;
+
+
+    [Header("Rotacion")]
+    [SerializeField]
+    private float rotationF1;
+    [SerializeField]
+    private float rotationF2;
+    [SerializeField]
+    private float smoothTime;
+    private Vector3 desiredPosition = Vector3.zero;
+
 
     void FixedUpdate()
     {
-        // Calcular la posición deseada de la cámara
-        Vector3 desiredPosition = new Vector3(target.position.x, distanceY, target.position.z + distanceZ);
-
-        // Actualizar la posición de la cámara utilizando SmoothDamp
-        transform.position = Vector3.SmoothDamp(transform.position, desiredPosition, ref velocity, smoothTime);
-
-        // Actualizar la rotación de la cámara
-        transform.rotation = Quaternion.Euler(xRotation, transform.eulerAngles.y, 0f);
+        MoveCamera();
+        RotateCamera();
     }
+
+    private void RotateCamera()
+    {
+        Vector2 mousePosition = Input.mousePosition;
+        float min = Screen.height / 2;
+        float max = 0;
+        float positionLerp = Mathf.InverseLerp(min, max, mousePosition.y);
+        float rotationMin = 52;
+        float rotationMax = 60;
+
+        if (mousePosition.y < Screen.height / 2)
+        {
+            xRotation = Mathf.Lerp(rotationMin, rotationMax, positionLerp);
+        }
+        Quaternion currentRotation = Quaternion.Euler(xRotation, transform.eulerAngles.y, 0f);
+        transform.rotation = Quaternion.Lerp(transform.rotation, currentRotation, rotationLerp);
+
+    }
+
+    private void MoveCamera()
+    {
+        Vector2 mousePosition = Input.mousePosition;
+        float screenWidthThird = Screen.width / 3;
+
+        if (mousePosition.y < Screen.height / 2)
+        {
+            if (mousePosition.x < screenWidthThird)
+            {
+                desiredPosition = new Vector3(target.position.x - rotationF1, distanceY, target.position.z + distanceZ + rotationF2);
+            }
+            else if (mousePosition.x > screenWidthThird * 2)
+            {
+                desiredPosition = new Vector3(target.position.x + rotationF1, distanceY, target.position.z + distanceZ + rotationF2);
+            }
+            else
+            {
+                desiredPosition = new Vector3(target.position.x, distanceY, target.position.z + distanceZ );
+            }
+        }
+        else if (mousePosition.y > Screen.height / 2)
+        {
+            float rotation1 = rotationF1 / 3.4f;
+            float rotation2 = rotationF2 / 3.4f;
+            if (mousePosition.x < Screen.width / 2)
+            {
+                desiredPosition = new Vector3(target.position.x - rotation1, distanceY, target.position.z + distanceZ + rotation2);
+            }
+            else
+            {
+                desiredPosition = new Vector3(target.position.x + rotation1, distanceY, target.position.z + distanceZ + rotation2);
+            }
+        }
+        transform.position = Vector3.SmoothDamp(transform.position, desiredPosition, ref velocity, smoothTime);
+    }
+
 }
--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -5,12 +5,16 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     [SerializeField]
-    private float m_speed;
-    [SerializeField]
-    private float r_speed;
+    private float walk_speed;
     [SerializeField]
     private float back_speed;
+    [SerializeField]
+    private float run_speed;
+    [SerializeField]
+    private float rotation_speed;
     private Rigidbody rb;
+    [SerializeField]
+    Animator animator;
 
     private void Start()
     {
@@ -25,10 +29,29 @@ public class Player : MonoBehaviour
         float axisHorizontal = Input.GetAxisRaw("Horizontal");
         float axisVertical = Input.GetAxisRaw("Vertical");
         Vector3 direction = new Vector3(axisHorizontal , 0f, axisVertical).normalized;
-        float currentSpeed = m_speed;
-        if ((Vector3.Angle(direction, transform.forward) <= 70.0f) == false)
+        float currentSpeed = walk_speed;
+
+        if (direction == Vector3.zero)
         {
+            animator.SetInteger("Walk", 0);
+        }
+
+        else if ((Vector3.Angle(direction, transform.forward) <= 70.0f) == false)
+        {
+            animator.SetInteger("Walk", -1);
             currentSpeed = back_speed;
+        }
+        else
+        {
+            if (Input.GetKey(KeyCode.LeftShift))
+            {
+                animator.SetInteger("Walk", 2);
+                currentSpeed = run_speed;
+            }
+            else
+            {
+                animator.SetInteger("Walk", 1);
+            }
         }
         rb.MovePosition(transform.position + direction * currentSpeed * Time.deltaTime);
 
@@ -39,11 +62,12 @@ public class Player : MonoBehaviour
         {
             Vector3 target = new Vector3(hit.point.x, transform.position.y, hit.point.z);
             Quaternion targetRotation = Quaternion.LookRotation(target - transform.position);
-            transform.rotation = Quaternion.Lerp(transform.rotation, targetRotation, r_speed * Time.deltaTime);
+            transform.rotation = Quaternion.Lerp(transform.rotation, targetRotation, rotation_speed * Time.deltaTime);
         }
     }
     private void initializateStart()
     {
         rb = transform.GetComponent<Rigidbody>();
+        animator = transform.GetComponent<Animator>(); 
     }
 }
